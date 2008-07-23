157e6b2152dfa94c162ccc4ee0590e86374bb50a
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-23 08:22:42 +0000

    2.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/loginusertrack/trunk@9505 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "loginusertrack"
 #
-# Auto generated 25-04-2008 12:49
+# Auto generated 23-07-2008 11:20
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Login User Tracking',
-	'description' => 'Logs in a separate table each time a fronend user logs in and further the timespan of the session and viewed pages. Backend module provides statistics over the data.',
+	'description' => 'Logs in a separate table each time a frontend user logs in and further the timespan of the session and viewed pages. Backend module provides statistics over the data.',
 	'category' => 'module',
 	'shy' => 0,
 	'dependencies' => '',
@@ -26,12 +26,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Kasper Skårhøj',
-	'author_email' => 'kasper@typo3.com',
-	'author_company' => 'Curby Soft Multimedia',
+	'author' => 'Kasper Skaarhoj / Dmitry Dulepov [netcreators]',
+	'author_email' => 'dmitry@typo3.org',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.1.2',
+	'version' => '2.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.1.0-0.0.0',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"8a7b";s:36:"class.tx_loginusertrack_tsfehook.php";s:4:"e2fe";s:21:"class.ux_tslib_fe.php";s:4:"2e88";s:12:"ext_icon.gif";s:4:"0bef";s:17:"ext_localconf.php";s:4:"9936";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"10c0";s:16:"locallang_db.xml";s:4:"2b7d";s:7:"tca.php";s:4:"53a5";s:14:"doc/manual.sxw";s:4:"a0e2";s:42:"mod1/class.tx_loginusertrack_lastlogin.php";s:4:"ba94";s:42:"mod1/class.tx_loginusertrack_pagestats.php";s:4:"fab0";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2c0e";s:14:"mod1/index.php";s:4:"2840";s:18:"mod1/locallang.xml";s:4:"2fd4";s:22:"mod1/locallang_mod.xml";s:4:"ec16";s:19:"mod1/moduleicon.gif";s:4:"0bef";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"d253";s:36:"class.tx_loginusertrack_tsfehook.php";s:4:"e2fe";s:12:"ext_icon.gif";s:4:"0bef";s:17:"ext_localconf.php";s:4:"e15d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"10c0";s:16:"locallang_db.xml";s:4:"2b7d";s:7:"tca.php";s:4:"53a5";s:14:"doc/manual.sxw";s:4:"a0e2";s:42:"mod1/class.tx_loginusertrack_lastlogin.php";s:4:"ba94";s:42:"mod1/class.tx_loginusertrack_pagestats.php";s:4:"fab0";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2c0e";s:14:"mod1/index.php";s:4:"de53";s:18:"mod1/locallang.xml";s:4:"2fd4";s:22:"mod1/locallang_mod.xml";s:4:"ec16";s:19:"mod1/moduleicon.gif";s:4:"0bef";}',
 	'suggests' => array(
 	),
 );
