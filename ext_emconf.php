7b3564d304cf69efbd9b052f65499bd136ebf374
minderdl <minderdl@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-05 22:13:02 +0000

    TER version 2.0.7 - compatible with TYPO3 6.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/loginusertrack/trunk@72273 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "loginusertrack".
  *
- * Auto generated 27-02-2013 14:47
+ * Auto generated 05-03-2013 23:10
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -34,15 +34,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.1.0-0.0.0',
 			'php' => '4.4.0-0.0.0',
+			'typo3' => '4.1.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"d855";s:36:"class.tx_loginusertrack_tsfehook.php";s:4:"d60a";s:12:"ext_icon.gif";s:4:"0bef";s:17:"ext_localconf.php";s:4:"e15d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"402c";s:16:"locallang_db.xml";s:4:"0e8f";s:7:"tca.php";s:4:"ae12";s:14:"doc/manual.sxw";s:4:"a0e2";s:42:"mod1/class.tx_loginusertrack_lastlogin.php";s:4:"9204";s:42:"mod1/class.tx_loginusertrack_pagestats.php";s:4:"fab0";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2c0e";s:14:"mod1/index.php";s:4:"6bd8";s:18:"mod1/locallang.xml";s:4:"0ead";s:22:"mod1/locallang_mod.xml";s:4:"ec16";s:19:"mod1/moduleicon.gif";s:4:"0bef";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"2fa6";s:36:"class.tx_loginusertrack_tsfehook.php";s:4:"bfbb";s:12:"ext_icon.gif";s:4:"0bef";s:17:"ext_localconf.php";s:4:"e15d";s:14:"ext_tables.php";s:4:"2003";s:14:"ext_tables.sql";s:4:"10c0";s:16:"locallang_db.xml";s:4:"2b7d";s:7:"tca.php";s:4:"53a5";s:14:"doc/manual.sxw";s:4:"a0e2";s:42:"mod1/class.tx_loginusertrack_lastlogin.php";s:4:"f1ec";s:42:"mod1/class.tx_loginusertrack_pagestats.php";s:4:"fab0";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"1dc3";s:14:"mod1/index.php";s:4:"8e48";s:18:"mod1/locallang.xml";s:4:"2fd4";s:22:"mod1/locallang_mod.xml";s:4:"ec16";s:19:"mod1/moduleicon.gif";s:4:"0bef";}',
 	'suggests' => array(
 	),
 );
